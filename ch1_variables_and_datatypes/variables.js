a468c6dd9efbea70eaefe255c96080b55de0f477
Hinda <profhindo@gmail.com>
2025-07-19 23:30:09 -0700

    add the variables to javafile

---
+++ ch1_variables_and_datatypes/variables.js
@@ -0,0 +1,15 @@
+// var firstName = "Hinda" // string
+// var lastName = "Mohamoud" // string
+// var age= 33 // number
+// var city = "Hargaysa" // string
+// var isStudent =true // boolean
+
+
+// // console.log(firstName * lastName)
+// console.log(age * 2)
+
+var magacaKobaad5 = 3;// declaring a variable
+// firstName = "Ahmed" // assigning a value, initializing the variable
+// firstName = "Hinda"
+console.log(typeof magacaKobaad5); 
+
