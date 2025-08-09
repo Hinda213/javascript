6738e0daf0e3c789f120f6e1b653bef1c9ef3109
Hinda <profhindo@gmail.com>
2025-08-09 08:05:30 -0700

    I have learnt arrays  and some strings

---
+++ arrays/array.js
@@ -0,0 +1,14 @@
+// let firstNames = ["Hinda", "Ahmed", "Mohamed", "Ayan", "Bille"];
+// firstNames[2] = "Fartun"
+// firstNames.pop()
+// firstNames.push("Fatma" , " Khadra")
+// firstNames.unshift("Anaz" , " Leyla")
+// firstNames.shift()
+// let newarray = firstNames.concat(["Hanad" , "Hani"])
+// console.log(newarray.length)
+let firstNames = ["Hinda", "Ahmed", "Mohamed", "Ayan", "Bille"];
+// let newFirstNames = firstNames.slice(1 , 4)
+firstNames.splice(1 , 3 , "Kaltun" , "Muna" , "Geele") 
+
+// console.log(newFirstNames);
+console.log(firstNames);
